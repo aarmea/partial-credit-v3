8999fe83b54efa8657ecd94776c886ea4f5779ac
Albert Armea <albert@albertarmea.com>
2014-01-02 20:28:42 -0500

    Fix profile photo upload indentation

--- public/templates/profile_photo_edit.tpl.php
+++ public/templates/profile_photo_edit.tpl.php
@@ -8,7 +8,7 @@ $profileEdited = $currentUser;
     <h1>Edit Profile Picture: <?=$profileEdited->rcsid()?></h1>
     <p>This picture is displayed publicly on the "About Us" page:</p>
     <img src="<?=$profileEdited->photoURL()?>" alt="<?=$profileEdited->fullName()?>">
-          <p>The new image must be a JPEG (*.jpg) file smaller than 1 MB.</p>
+    <p>The new image must be a JPEG (*.jpg) file smaller than 1 MB.</p>
     <form enctype="multipart/form-data" action="post.php" method="post">
       <input type="hidden" name="rcsid" value="<?=$profileEdited->rcsid()?>">
       <input type="hidden" name="MAX_FILE_SIZE" value="1048576">
