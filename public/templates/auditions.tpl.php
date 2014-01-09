572f15250ff224589074f42d702a0b10c9d65618
Albert Armea <albert@albertarmea.com>
2014-01-09 17:30:21 -0500

    Fill in the auditions page

---
+++ public/templates/auditions.tpl.php
@@ -0,0 +1,23 @@
+<div class="row">
+  <div class="small-12 columns">
+    <h1>Auditions</h1>
+    <p>Auditions for Partial Credit are held once per semester. Come check
+      us out at the activities fair to sign up for an audition time or look
+      for fliers posted around campus for information about audition
+      opportunities!</p>
+    <p>During auditions, we'll ask you to:
+      <ul>
+        <li>Go up and down scales to warm up and test your range</li>
+        <li>Do a volume exercise (crescendo and decrescendo on a single
+          pitch)</li>
+        <li>Match patterns of notes played on a piano</li>
+        <li>Perform one to two minutes of a song of your choice (usually a
+          verse and a chorus)</li>
+        <li>Do some impromptu vocal percussion if you can</li>
+      </ul>
+    </p>
+    <p>Just remember to relax, do your best, and have fun!
+      <a href="?p=contact">Contact us</a> if you have any questions and to sign
+      up for an audition time slot.</p>
+  </div>
+</div>
