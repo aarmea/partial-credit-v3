01c94fd3e8a5816b22a7b5f9948b334b29a9d207
Albert Armea <albert@albertarmea.com>
2014-01-09 16:43:26 -0500

    Turn off CAS debug mode

--- public/php/auth/cas_init.php
+++ public/php/auth/cas_init.php
@@ -2,9 +2,6 @@
 // CAS initialization for logged-in pages
 require_once "config.php";
 
-// TODO: Switch this off for production
-phpCAS::setDebug();
-
 phpCAS::client(CAS_VERSION_2_0, $CAS_HOST, $CAS_PORT, $CAS_URI);
 phpCAS::setCasServerCACert($CAS_CA_CERT_PATH);
 
